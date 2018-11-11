dfc1e944665f9e4b9dcaa92f1c4a19f4fa83bcfd
ogkushaladaora <37936157+ogkushaladaora@users.noreply.github.com>
2018-11-11 15:51:07 -0500

    Create UserPageTop.php

---
+++ UserPage/UserPageTop.php
@@ -0,0 +1,81 @@
+<!DOCTYPE=HTML>
+<html lang ="en">
+<head>
+	<!--Needs:
+		Website Name
+		Link to index
+		styles?  -->
+	<title><?php echo $title; ?></title>
+	<style type="text/css">
+		html, body {
+		    height: 100%;
+		    width:  100%;
+		}
+		html {
+			background-color: #fff2e6;
+		}
+		body {
+		    margin: 10px; /* This will stop the margin, setting it to 0 */
+		}
+		div {
+			margin: 10px;
+		}
+		#Title {
+			font-family: Georgia, serif;
+			font-size: 25px;
+			letter-spacing: 1px;
+			word-spacing: 0px;
+			color: #000000;
+			font-weight: normal;
+			text-decoration: none;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+		}
+		#User {
+			font-family: Arial, Helvetica, sans-serif;
+			font-size: 20px;
+			letter-spacing: 1px;
+			word-spacing: 0px;
+			color: #000000;
+			font-weight: normal;
+			text-decoration: none;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+		}
+		#Par {
+			font-family: Arial, Helvetica, sans-serif;
+			font-size: 15px;
+			letter-spacing: 1px;
+			word-spacing: 0px;
+			color: #000000;
+			font-weight: normal;
+			text-decoration: none;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+		}
+		#Body {
+			background: #CE7673;
+			background: -moz-linear-gradient(top, #CE7673 0%, #C5B296 100%);
+			background: -webkit-linear-gradient(top, #CE7673 0%, #C5B296 100%);
+			background: linear-gradient(to bottom, #CE7673 0%, #C5B296 100%);
+		}
+		.overflow-wrap {
+			overflow-wrap: break-word;
+		}
+	</style>
+</head>
+
+<body>
+
+	<a href="index.html">
+		<div id="Title">
+			Home
+		</div>
+	</a>
+
+<!-- </body> -->
+
+</html>
