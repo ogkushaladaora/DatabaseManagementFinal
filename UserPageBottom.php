061eeef635d5b057983414781b27ff137cf8e453
ogkushaladaora <37936157+ogkushaladaora@users.noreply.github.com>
2018-11-11 15:49:24 -0500

    Delete UserPageBottom.php

--- UserPageBottom.php
+++
@@ -1,9 +0,0 @@
-<!--Needs:
-
-	home | about | contact us
-
--->
-
-<div style="text-align: center"><a href="index.php">Home</a> | <a href="About.html">About</a> | <a href="Contact.html">Contact</a></div>
-
-</body>
