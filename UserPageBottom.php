b45b11e3583f2c01ed2f54616e8fe3e22546e6a6
ogkushaladaora <37936157+ogkushaladaora@users.noreply.github.com>
2018-11-11 15:47:40 -0500

    Add files via upload

---
+++ UserPageBottom.php
@@ -0,0 +1,9 @@
+<!--Needs:
+
+	home | about | contact us
+
+-->
+
+<div style="text-align: center"><a href="index.php">Home</a> | <a href="About.html">About</a> | <a href="Contact.html">Contact</a></div>
+
+</body>
